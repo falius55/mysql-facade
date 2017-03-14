e49d2912c9bd4ca5f6200659d9b00359292722db
falius on ideapad-ubuntu <ciero639@gmail.com>
2017-03-14 21:17:04 +0900

    javadocの出力先変更、docコメントの補強など

--- src/main/java/mysqlfacade/DatabaseColumn.java
+++ src/main/java/mysqlfacade/DatabaseColumn.java
@@ -1,10 +1,31 @@
 package mysqlfacade;
 
+/**
+ * データベースの列を表すインタフェースです。
+ */
 public interface DatabaseColumn {
-    public String tableNameMethod = "tableName";
+    public static final String TABLE_NAME_METHOD = "tableName";
 
+    /**
+     * その列の値のデータ型を文字列で返します。
+     * <pre>
+     * {@code
+     * "varchr(256)"
+     * "int"
+     * "enum('male', 'female')"
+     * "datetime"
+     * }
+     * </pre>
+     */
     String type();
 
+    /**
+     * その列を作成する際に、CREATE TABLE文に渡す文字列を返します。<br>
+     * 例：<br>
+     * {@code
+     * "name varchar(256) not null unique key"
+     * }
+     */
     String columnString();
 
     /**
